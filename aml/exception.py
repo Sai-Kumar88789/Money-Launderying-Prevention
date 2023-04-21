39a86af5195142736e1a3d2567ef506281e5381e
saikumar <sai680513@gmail.com>
2023-04-21 12:35:28 +0530

    Build API using Fastapi

--- aml/exception.py
+++ aml/exception.py
@@ -12,6 +12,8 @@ def error_message_detail(error, error_detail: sys):
 
     return error_message
 
+class AMLException(BaseException):
+    pass
 
 class AMLException(Exception):
     def __init__(self, error_message, error_detail:sys):
--- main.py
+++ main.py
@@ -1,4 +1,104 @@
-from aml.pipelines.training_pipeline import TrainPipeline
+# from aml.pipelines.training_pipeline import TrainPipeline
 
-train = TrainPipeline()
-train.run_pipeline()
+# train = TrainPipeline()
+# train.run_pipeline()
+
+from aml.configuration.mongodb_connection import MongodbClient
+from aml.exception import AMLException
+import os,sys
+from aml.logger import logging
+from aml.pipelines import training_pipeline
+from aml.pipelines.training_pipeline import TrainPipeline
+import os
+from aml.utils.main_utils import read_yaml_file
+from aml.constant.training_pipeline import SAVED_MODEL_DIR
+from fastapi import FastAPI, File, UploadFile,Request
+from aml.constant.application import APP_HOST, APP_PORT
+from starlette.responses import RedirectResponse
+from uvicorn import run as app_run
+from fastapi.responses import Response , StreamingResponse,HTMLResponse
+from aml.ml.model.estimator import ModelResolver,TargetValueMapping
+from aml.utils.main_utils import load_object
+from fastapi.middleware.cors import CORSMiddleware
+import os
+import pandas as pd
+import io
+
+
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGODB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGODB_URL']=env_config['MONGODB_URL']
+
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def train_route():
+    try:
+
+        train_pipeline = TrainPipeline()
+        if train_pipeline.is_pipeline_running:
+            return Response("Training pipeline is already running.")
+        train_pipeline.run_pipeline()
+        return Response("Training successful !!")
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+
+@app.post("/predict")
+async def predict_route(request:Request,file: UploadFile = File(...)):
+    try:
+        #get data from user csv file
+        #conver csv file to dataframe
+        #df = pd.read_csv(file.file)
+        df = pd.read_csv(io.StringIO(file.file.read().decode("utf-8")))
+
+        model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
+        if not model_resolver.is_model_exist():
+            return Response("Model is not available")
+        
+        best_model_path = model_resolver.get_best_model_path()
+        model = load_object(file_path=best_model_path)
+        y_pred = model.predict(df)
+        df['predicted_column'] = y_pred
+        df['predicted_column'].replace(TargetValueMapping().reverse_mapping(),inplace=True)
+        #return df.to_html()
+        #decide how to return file to user.
+        html = df.to_html()
+        return HTMLResponse(content=html)
+
+        
+    except Exception as e:
+        return {"error": str(e)}
+
+# def main():
+#     try:
+#         set_env_variable(env_file_path)
+#         training_pipeline = TrainPipeline()
+#         training_pipeline.run_pipeline()
+#     except Exception as e:
+#         print(e)
+#         logging.exception(e)
+
+
+if __name__=="__main__":
+    #main()
+    #set_env_variable(env_file_path)
+    app_run(app, host=APP_HOST, port=APP_PORT)
